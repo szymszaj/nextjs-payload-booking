874d7253bf4d0797efcb99303e011b3e90241c95
Szymon Zych <szymonzych868@gmail.com>
2026-06-07 22:35:53 +0200

    feat: update Next.js configuration and scripts for Payload CMS integration

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,20 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+import { withPayload } from '@payloadcms/next/withPayload'
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default withPayload(nextConfig)
